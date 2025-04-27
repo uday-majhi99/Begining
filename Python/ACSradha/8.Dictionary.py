8ffc003428be6f74f06a8b298a89b3bfbbc153f6
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-04-27 18:30:31 +0545

    Started dictionaries

---
+++ Python/ACSradha/8.Dictionary.py
@@ -0,0 +1,18 @@
+"""
+Dictionaries are used to store data values in key:value pairs
+They are unordered,mutable(changeable) and don't allow duplicate keys
+dict = {
+    "key" : "values",
+    "key" : "values"
+"""
+
+this_dict = {
+    "name" : "Uday Majhi",
+    "age" : 26,
+    "address" : "Lalitpur",
+    "is_adult" : True,
+    "fav_num" : 20.2,
+    "languages" : ["Python","C","Java"],
+    "tuples_here" : ("Hello","This","is","a","tuple")
+}
+print(this_dict)
