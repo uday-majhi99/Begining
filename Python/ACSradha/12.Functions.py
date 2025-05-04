b670b9f60e3d5fa337a9c8656ea075e6b2bb8c3e
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-05-04 23:31:21 +0545

    Statred

---
+++ Python/ACSradha/12.Functions.py
@@ -0,0 +1,17 @@
+"""
+Block of statements that perform a specific task.
+def func_name(param1,param2):(Defining function)
+    some_work
+    return value
+
+def func_name(arg1,arg2) - Function call
+"""
+
+def func(a,b):
+    sum = a+b
+    print(sum)
+func(1,2)
+func(3,4)
+func(4,5)
+func(6,6)
+func(7,7)
