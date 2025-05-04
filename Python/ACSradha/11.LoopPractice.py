32dc855bc16ad0745bb1f9a5b39b9e604fc0b90a
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-05-04 23:26:09 +0545

    Practiced successfully

--- Python/ACSradha/11.LoopPractice.py
+++ Python/ACSradha/11.LoopPractice.py
@@ -1,15 +1,51 @@
-squares = []
+# squares = []
+#
+# for i in range(1,11):
+#     squares.append(i*i)
+#
+# print(f"List of elements is ",squares)
+# squaress = tuple(squares)
+# print(f"List of elements is ",squaress)
+#
+# odds = []
+# for j in range(1,100,2):
+#     odds.append(j)
+# print(odds)
+# lists = tuple(odds)
+# print(lists)
+#
+# even = []
+# for o in range(0,100,2):
+#     even.append(o)
+# print(even)
+#
+# hundred = []
+# for o in range(1,100,1):
+#     hundred.append(o)
+# print(hundred)
+#
+# hundreds = []
+# for o in range(100,1,-1):
+#     hundreds.append(o)
+# print(hundreds)
+#
+# multi = int(input("Enter a number for multiplication:"))
+# multip = []
+# for i in range(1,11,1):
+#     multip.append(multi*i)
+#
+# print(multip)
 
-for i in range(1,11):
-    squares.append(i*i)
+#Sum of first n number
+first_number = int(input("Enter a number for sum: "))
+sum = 0
+for i in range(1,first_number,1):
+    sum += i
+print(sum)
 
-print(f"List of elements is ",squares)
-squaress = tuple(squares)
-print(f"List of elements is ",squaress)
-
-odds = []
-for j in range(1,100,2):
-    odds.append(j)
-print(odds)
-lists = tuple(odds)
-print(lists)
+#factorail of first n number
+factorial_num = int(input("Enter a number for factorial: "))
+sum = 0
+for i in range(1,factorial_num,1):
+    factorial_num *= i
+print(factorial_num)
