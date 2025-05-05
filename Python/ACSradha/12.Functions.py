9d4d4f5023d33826dd56ac8a5041c6d8142ee2b0
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-05-05 22:26:51 +0545

    Started learning function

--- Python/ACSradha/12.Functions.py
+++ Python/ACSradha/12.Functions.py
@@ -7,11 +7,3 @@ def func_name(param1,param2):(Defining function)
 def func_name(arg1,arg2) - Function call
 """
 
-def func(a,b):
-    sum = a+b
-    print(sum)
-func(1,2)
-func(3,4)
-func(4,5)
-func(6,6)
-func(7,7)
