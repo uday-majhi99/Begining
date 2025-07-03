bfdbb756773da07e95a5d371baf0992c0db9ea58
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-07-03 07:50:54 +0545

    Added functions like kwargs

--- Python/ACSradha/12.Functions.py
+++ Python/ACSradha/12.Functions.py
@@ -7,3 +7,32 @@ def func_name(param1,param2):(Defining function)
 def func_name(arg1,arg2) - Function call
 """
 
+
+def sum_calc(a,b):
+    return a+b
+
+sums = sum_calc(2,3)
+print(sums)
+
+def func():
+    return "Hello"
+
+functi = func()
+print(functi)
+
+def add_all(*args):
+    return sum(args)
+
+print(add_all(1, 2, 3, 4))
+
+def print_info(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+
+print_info(name = "Uday Majhi", age = 27)
+
+def infor(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+
+infor(city = "Biratnagar", Zone = "Koshi", Village = "Sakhuware")
