73e44ab679defb9dd2e8d9dc872595dd42195e0d
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-07-13 17:16:36 +0545

    Learned about functions

--- Python/ACSradha/Notes/13.Function2.py
+++ Python/ACSradha/Notes/13.Function2.py
@@ -4,3 +4,53 @@ def calc(a,b):
     return sum
 
 calc(2,3)
+
+def ad_sum(a, b):
+    if a == 0:
+        return None
+    return a + b
+
+sums = ad_sum(2, 3)
+print(sums)
+
+print("Hello this", end= "  ")
+print("Uday")
+
+def calculate_mul(a=2,b=2):
+    print(a*b)
+    return a * b
+
+calculate_mul()
+
+cities = ["Kathmandu","Lalitpur","Bhaktapur"]
+villages = ["Sakhuware","Aurabani","Bhamari"]
+
+def prin_list(list):
+    print(len(list))
+
+def print_list1(list):
+    for item in list:
+        print(item, end = " ")
+
+# print_list1(cities)
+#
+# num = 5
+# fact = 1
+# for i in range(1,num+1):
+#     fact *= i
+#
+# print("\n",fact)
+
+def cal_fact():
+    num = 4
+    fact = 1
+    for i in range(1,num+1):
+        fact *= i
+    print(fact)
+    return fact
+
+print(cal_fact())
+
+#  USD to NRS
+
+
