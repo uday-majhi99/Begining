3f233515206c4d7c033b563333fced3514cc48b0
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-07-27 18:23:43 +0545

    Added IO and started learning it

--- Python/ACSradha/14.Recursion.py
+++ Python/ACSradha/14.Recursion.py
@@ -1,7 +1,31 @@
-def show(n):
-    if n == 0:
-        return n
-    print(n)
-    show(n-1 )
+# def show(n):
+#     if n == 0:
+#         return n
+#     print(n)
+#     show(n-1 )
+#
+# show(5)
 
-show(5)
+# def show(n):
+#         if(n==0):
+#             return
+#         print(n)
+#         show(n-1)
+#         print("End")
+#
+# show(5)
+
+# def fact(num):
+#     result = 1
+#     for i in range(1, num + 1):
+#         result *= i
+#     print(result)
+#
+# fact(3)
+
+def fact(n):
+    if (n==1 or n==0):
+        return 1
+    return fact(n-1)*n
+
+print(fact(3))
