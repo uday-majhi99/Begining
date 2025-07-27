6fb2cdef6013fb3d365d36d95382a50b1a80bf33
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-07-27 22:49:04 +0545

    learning file handling

---
+++ Python/ACSradha/15.IO.py
@@ -0,0 +1,15 @@
+# Read files
+f = open("fileinputoutput.txt", "r")
+dates = f.read(3)
+data = f.read()
+print(dates)
+print(data)
+print(type(data))
+f.close()
+
+fi = open("/Users/udaymajhi/Documents/Begining/Begining/Python/ACSradha/Notes/demo.txt","r")
+datas = fi.read()
+print(datas)
+fi.close()
+
+#
