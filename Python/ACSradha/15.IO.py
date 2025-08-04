b71a8e2e61442b53a875749870a3cc3a7dfdda90
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-08-04 21:36:18 +0545

    learning file handing

--- Python/ACSradha/15.IO.py
+++ Python/ACSradha/15.IO.py
@@ -1,14 +1,40 @@
 # Read files
 f = open("fileinputoutput.txt", "r")
-dates = f.read(3)
+# dates = f.read(3)
 data = f.read()
-print(dates)
+# print(dates)
 print(data)
-print(type(data))
+# print(type(data))
+f.close()
+#
+# fi = open("/Users/udaymajhi/Documents/Begining/Begining/Python/ACSradha/Notes/demo.txt","r")
+# datas = fi.read()
+# print(datas)
+# fi.close()
+
+# g = open("fileinputoutput.txt","r")
+# # inp = g.read()
+# line1 = g.readline()
+# # print(inp)
+# print(line1)
+# line2 = g.readline()
+# print(line2)
+# g.close()
+
+# Write a data in a file
+f = open("fileinputoutput.txt", "w")
+f.write("hello is the learning input and output")
 f.close()
 
-fi = open("/Users/udaymajhi/Documents/Begining/Begining/Python/ACSradha/Notes/demo.txt","r")
-datas = fi.read()
-print(datas)
-fi.close()
+# Append(add) data in a file
+f = open("fileinputoutput.txt","a")
+f.write("\nThis line are added line")
+f.close()
+
+# Create a new file
+f = open("newfile.txt","w")
+dat = f.write("Hello")
+print(dat)
+f.close()
+
 
