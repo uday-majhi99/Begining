d1e661843d3e5fbb266893bc7d012e80bf355629
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2025-08-15 23:50:49 +0545

    Learned about methods ,constructor and attributes

--- Python/ACSradha/15.OOPs.py
+++ Python/ACSradha/15.OOPs.py
@@ -18,18 +18,35 @@ print(s1.faculty)
 # f1 = factory()
 
 # Constructor
-class factory:
-    # Default constructor
-    def __ini__(self):
-        pass
+# class factory:
+#     # Default constructor
+#     def __ini__(self):
+#         pass
+#
+#     # Parameterized constructor
+#     def __init__(self,fullname,staff_no):
+#         self.name = fullname  #attributes
+#         self.staff_no = staff_no
+#
+# #Creating Object
+# f1 = factory("Uday","20")
+# print(f1.name,f1.staff_no)
 
-    # Parameterized constructor
-    def __init__(self,fullname,staff_no):
-        self.name = fullname
-        self.staff_no = staff_no
 
-#Creating Object
-f1 = factory("Uday","20")
-print(f1.name,f1.staff_no)
+# class car:
+#     def __init__(self,car_color,car_version):
+#         self.car_color = car_color
+#         self.car_version =  car_version
+#
+#     def welcome(self):
+#         print("Heelo world",self.car_color)
+#
+#     def get_marks(self):
+#         return self.car_version
+#
+# c1 = car("Red","V4")
+# print(c1.car_color,c1.car_version)
+#
+# c1.welcome()
+# print(c1.get_marks())
 
-# Attributes
